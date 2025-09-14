e9b2056b040733c93d542796796a1e3ae2649c68
Matheus Amorim <matheusfelix.amorim@hotmail.com>
2025-09-14 12:38:21 -0300

    feat: adicionado suspense para loading de posts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
+import { PostsList } from "@/components/PostsList";
 import { SpinLoader } from "@/components/SpinLoader";
+import { Suspense } from "react";
 
-export default function HomePage() {
+export default async function HomePage() {
   return (
     <div>
-      <SpinLoader  />
+      <h1 className="text-6xl font-bold text-center py-8">Aqui é a header</h1>
+      <Suspense fallback={<SpinLoader />}>
+        <PostsList/>
+      </Suspense>
+      <footer>
+        <p className="text-6xl font-bold text-center py-8">Footer</p>
+      </footer>
     </div>
   );
 }
