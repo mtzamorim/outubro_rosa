6d9f0a935529513ad2b63d1627e802c975a6aa9c
Matheus Amorim <matheusfelix.amorim@hotmail.com>
2025-09-15 20:27:00 -0300

    feat: adicionado tema claro e escuro

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,13 @@ import { Suspense } from "react";
 
 export default async function HomePage() {
   return (
-    <div>
+    <div className='text-slate-900 bg-slate-100 min-h-screen dark:bg-slate-900 dark:text-slate-100'>
       <h1 className="text-6xl font-bold text-center py-8">Aqui é a header</h1>
+
       <Suspense fallback={<SpinLoader />}>
         <PostsList/>
       </Suspense>
+
       <footer>
         <p className="text-6xl font-bold text-center py-8">Footer</p>
       </footer>
