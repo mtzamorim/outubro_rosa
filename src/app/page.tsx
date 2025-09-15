f37c5f25f32954b45f5815a38f409e6bc34d88cb
Matheus Amorim <matheusfelix.amorim@hotmail.com>
2025-09-15 20:58:06 -0300

    refactor: header com responsividade

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { Container } from "@/components/Container";
+import { Header } from "@/components/Header";
 import { PostsList } from "@/components/PostsList";
 import { SpinLoader } from "@/components/SpinLoader";
 import { Suspense } from "react";
@@ -6,7 +7,7 @@ import { Suspense } from "react";
 export default async function HomePage() {
   return (
   <Container>
-        <h1 className="text-6xl font-bold text-center py-8">Aqui é a header</h1>
+       <Header/>
 
         <Suspense fallback={<SpinLoader />}>
           <PostsList />
