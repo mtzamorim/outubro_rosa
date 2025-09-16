c45eaa7a36660374204860660406261b43e51936
Matheus Amorim <matheusfelix.amorim@hotmail.com>
2025-09-15 21:26:49 -0300

    feat: criado layout inicial da seção com fotos e texto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,20 +2,56 @@ import { Container } from "@/components/Container";
 import { Header } from "@/components/Header";
 import { PostsList } from "@/components/PostsList";
 import { SpinLoader } from "@/components/SpinLoader";
+import Image from "next/image";
+import Link from "next/link";
 import { Suspense } from "react";
 
 export default async function HomePage() {
   return (
-  <Container>
-       <Header/>
+    <Container>
+      <Header />
 
-        <Suspense fallback={<SpinLoader />}>
-          <PostsList />
-        </Suspense>
+      <section className="grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group">
+        <Link className="w-full h-full overflow-hidden rounded-xl" href="#">
+          <Image
+            className="w-full h-full object-cover  object-center group-hover:scale-105 transition"
+            src="/images/Cartaz-Outubro-Rosa-simples-rosa-e-branco.png"
+            width={1200}
+            height={720}
+            alt="titulo"
+            priority
+          />
+        </Link>
+        <div className="flex flex-col gap-4 sm:justify-center">
+          <time
+            className="text-slate-600 block text-sm/tight"
+            dateTime="2025-04-20"
+          >
+            20/04/2025 10:00
+          </time>
 
-        <footer>
-          <p className="text-6xl font-bold text-center py-8">Footer</p>
-        </footer>
-     </Container>
+          <h1 className="text-2xl/tight font-extrabold sm:text-4xl">
+            <Link href="#">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit
+            </Link>
+          </h1>
+
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni,
+            harum sit, quis temporibus modi eos possimus neque iste quasi qui
+            enim autem? Consequatur deleniti minima obcaecati dolorum assumenda
+            ex aliquam?
+          </p>
+        </div>
+      </section>
+
+      <Suspense fallback={<SpinLoader />}>
+        <PostsList />
+      </Suspense>
+
+      <footer>
+        <p className="text-6xl font-bold text-center py-8">Footer</p>
+      </footer>
+    </Container>
   );
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,12 @@
+import Link from "next/link";
+
 export function Header() {
   return (
     <header>
       <h1
       className="text-4xl/normal font-extrabold py-8
       sm:text-5xl/normal sm:py-10 md:text-6xl/normal md:py-11 lg:text-7/normal lg:py-12">
-        <a href="#">Outubro Rosa</a>
+        <Link href="#">Outubro Rosa</Link>
       </h1>
     </header>
   );
